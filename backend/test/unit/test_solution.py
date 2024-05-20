c4bde62879d23e895a121ec9e033601e23cff32d
elstarkov <samuelward37@gmail.com>
2024-05-20 19:33:39 +0200

    Added unit tests

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,6 +1,35 @@
 import pytest
+import unittest.mock as mock
+from unittest.mock import patch
+from src.controllers.recipecontroller import RecipeController
+from src.static.diets import from_string
 
-# add your test case implementation here
+@pytest.fixture
+def sut():
+    items_dao= mock.MagicMock()
+
+    return RecipeController(items_dao=items_dao)
+
 @pytest.mark.unit
-def test():
-    pass
+def test_optimal_normal(sut):
+    with mock.patch.object(sut, 'get_readiness_of_recipes', return_value ={'normal': 0.11}):
+        res = sut.get_recipe(from_string("normal"), True)
+        assert res == 'normal'
+
+@pytest.mark.unit
+def test_not_optimal_random_return(sut):
+    with mock.patch.object(sut, 'get_readiness_of_recipes', return_value={'normal': 0.1, 'normal2': 0.12}):
+        res = sut.get_recipe(from_string("normal"), False)
+        assert res in ["normal", "normal2"]
+
+@pytest.mark.unit
+def test_low_readiness_value(sut):
+    with mock.patch.object(sut, 'get_readiness_of_recipes', return_value={'normal': 0.01}):
+        res = sut.get_recipe(from_string("normal"), False)
+        assert res == None
+
+@pytest.mark.unit
+def test_no_available_recipe(sut):
+    with mock.patch.object(sut, 'get_readiness_of_recipes', return_value ={}):
+        res = sut.get_recipe(from_string("normal"), True)
+        assert res == None
